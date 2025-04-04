0ebc56360b846acab4b072eda19e726a185e1eaa
wrnnrn <74679791+wrnnrn@users.noreply.github.com>
2025-04-04 09:46:39 +0200

    Update main.js
    Cargar DOM, identificar botones y guardarlos en un array.

--- js-learning/proyectos/calculadora/main.js
+++ js-learning/proyectos/calculadora/main.js
@@ -2,3 +2,14 @@
 La primera opción para saber si un elemento está siendo pulsado, es escuchar a este elemento contínuamente.
 Si fuera solo uno, sería factible, pero como son mas de 10, vamos a utilizar otra alternativa.
 */
+
+document.addEventListener("DOMContentLoaded", () => {
+	// Registar los números que se han pulsado y guardarlos en una variable
+	var numeros_pulsados = [];
+	document.addEventListener('click', (event) => {
+		if (event.target.tagName === "BUTTON") {
+			numeros_pulsados.push(event.target.id)
+			console.log(numeros_pulsados)
+		};
+	});
+});
