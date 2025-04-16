1abf985558d695c6c161000d8a94f97a2ed1b61e
wrnnrn <74679791+wrnnrn@users.noreply.github.com>
2025-04-16 10:36:46 +0200

    Update main.js

--- js-learning/proyectos/calculadora/main.js
+++ js-learning/proyectos/calculadora/main.js
@@ -10,20 +10,43 @@ ELEMENTOS:
 document.addEventListener('DOMContentLoaded', () => {
 	let elementos = [];
 	document.addEventListener('click', (event) => {
-		if(event.target.tagName === "BUTTON"){
+		if(event.target.tagName === "BUTTON" && event.target.id !== 'borrar' && event.target.id !== 'igual'){
+			event.preventDefault(); // Para evitar la acción de ctrl + p
 			elementos.push(event.target.id);
 			console.log(elementos);
 		};
 
+	let borrar = document.getElementById("borrar");
+	borrar.addEventListener('click', () => {
+		elementos.length = 0
+		console.log(elementos)
+	})
 		// Antes de la función para calcular el resultado, tenemos que hacer una función que identifique elementos en el array.
-		function find(elementoBuscar, elementoGuardado) {
-			elementoGuardado = elementos.indexOf(elementoBuscar);
+		function operar() {
+			// 1 - Encontrar elementos de jerarquía --> * y /
+
+			if(elementos.includes("*") || elementos.includes("/")) {
+				let multiplicacion = [];
+				multiplicacion = elementos.indexOf("*");
+				let division = [];
+				division = elementos.indexOf("/");
+				console.log('Multiplicación: ' + multiplicacion + ' División: ' + division);
+			}
+
+			// Funcion para buscar un elemento -> lista.indexOf(elemento)
+
 		}
 
+
 		// Funcion para calcular
-		function calcular() {
-			console.log("");
-		}
+		let igual = document.getElementById("igual")
+		igual.addEventListener('click', () => {
+			if (elementos.length == 0) {
+				print("No se ha pulsado ningún elemento");
+			} else {
+				operar();
+			}
+		})
 
 	});
 
